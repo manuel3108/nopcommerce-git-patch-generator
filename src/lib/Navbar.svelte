f1409f2f186d2508169867f213514978c038413e
Manuel Serret <mserret99@gmail.com>
2021-12-05 18:08:47 +0100

    use correct adapter

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -2,32 +2,10 @@
 	<div class="navbar-brand">
 		<div class="navbar-item">
 			<a href="/"><img src="/favicon.png" alt="Plugin Gleisbelegung Logo" /></a>
-			Plugin Gleisbelegung
+			NopCommerce Patch Generator
 		</div>
 	</div>
-	<div class="navbar-menu">
-		<div class="navbar-item">
-			<a href="/installation">Installation</a>
-		</div>
-		<div class="navbar-item">
-			<a href="/faq">FAQ</a>
-		</div>
-		<div class="navbar-item">
-			<a href="/changelog">Changelog</a>
-		</div>
-		<div class="navbar-end">
-			<div class="navbar-item">
-				<a href="https://github.com/gleisbelegung/gleisbelegung">Github</a>
-			</div>
-			<div class="navbar-item">
-				<span>
-					<!-- {#if $currentTime}
-						{$currentTime.toFormat('HH:mm:ss')}
-					{/if} -->
-				</span>
-			</div>
-		</div>
-	</div>
+	<div class="navbar-menu" />
 </nav>
 
 <style>
